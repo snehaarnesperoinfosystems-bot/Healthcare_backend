dc89eda7b1ed6e2e64e03b9167a0d276b891d33a
snehaarne_13 <snehaarne.speroinfosystems@gmail.com>
2026-06-29 13:17:27 +0530

    publish

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-hi
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Healthcare AI Report Analyzer",
+    description="AI-powered Healthcare Report Analyzer using FastAPI and MedGemma",
+    version="1.0.0"
+)
+
+
+@app.get("/")
+async def home():
+    return {
+        "status": "success",
+        "message": "Healthcare AI Backend is Running 🚀"
+    }
